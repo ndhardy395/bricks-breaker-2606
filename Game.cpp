eb997aa574d996a491ff8f0aba02597696b5509c
Noah Hardy <noahdhardy@outlook.com>
2026-08-03 19:11:18 -0400

    TODO #2 is finished for now. An external int x and int y are declared to change the brick positions, and currently we only make four bricks in a very simple row.

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "Game.h"
+#include <vector>
 
 Game::Game()
 {
@@ -20,24 +21,20 @@ void Game::Reset()
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
-	Box brick = {
-
-	brick.width = 10,
-	brick.height = 2,
-	brick.x_position = 0,
-	brick.y_position = 5,
-	brick.doubleThick = true,
-	brick.color = ConsoleColor::DarkGreen
-	};
-
-
-	std::vector<Box*> newBricks = {};
-	Box* newBrick = nullptr;
-
-
+	int x = 0;
+	int y = 5;
 	for (int i = 0; i < 4; ++i)
 	{
+	Box brick;
+	brick.width = 10;
+	brick.height = 2;
+	brick.x_position = x;
+	brick.y_position = y;
+	brick.doubleThick = true;
+	brick.color = ConsoleColor::DarkGreen;
 
+	bricks.push_back(brick);
+	x += 10;
 	}
 }
 
@@ -82,7 +79,10 @@ void Game::Render() const
 	ball.Draw();
 
 	// TODO #3 - Update render to render all bricks
-	brick.Draw();
+	for (const Box& b : bricks)
+	{
+	b.Draw();
+	}
 
 	Console::Lock(false);
 }
@@ -90,13 +90,19 @@ void Game::Render() const
 void Game::CheckCollision()
 {
 	// TODO #4 - Update collision to check all bricks
-	if (brick.Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
+	for (Box& b : bricks)
 	{
-		brick.color = ConsoleColor(brick.color - 1);
-		ball.y_velocity *= -1;
+		if (b.Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
+		{
+			b.color = ConsoleColor(b.color - 1);
+			ball.y_velocity *= -1;
 
-		// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
-
+			// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
+			if (b.color = ConsoleColor(0))
+			{
+				std::vector::erase(b);
+			}
+		}
 	}
 
 	// TODO #6 - If no bricks remain, pause ball and display (render) victory text with R to reset
--- Game.h
+++ Game.h
@@ -9,8 +9,8 @@ class Game
 	Box paddle = {};
 
 	// TODO #1 - Instead of storing 1 brick, store a vector of bricks (by value)
-	Box brick;
-	std::vector<Box> brickVector = {};
+	//Box brick;
+	std::vector<Box> bricks = {};
 
 public:
 	Game();
