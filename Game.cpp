e92e34fcc028d1acdc4d8b643243265fa48325ab
Noah Hardy <noahdhardy@outlook.com>
2026-08-03 18:36:52 -0400

    After doing some research, I decided it was safe to delete Box brick; and i also renamed brickVector to bricks

--- Game.cpp
+++ Game.cpp
@@ -20,12 +20,25 @@ void Game::Reset()
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
-	brick.width = 10;
-	brick.height = 2;
-	brick.x_position = 0;
-	brick.y_position = 5;
-	brick.doubleThick = true;
-	brick.color = ConsoleColor::DarkGreen;
+	Box brick = {
+
+	brick.width = 10,
+	brick.height = 2,
+	brick.x_position = 0,
+	brick.y_position = 5,
+	brick.doubleThick = true,
+	brick.color = ConsoleColor::DarkGreen
+	};
+
+
+	std::vector<Box*> newBricks = {};
+	Box* newBrick = nullptr;
+
+
+	for (int i = 0; i < 4; ++i)
+	{
+
+	}
 }
 
 void Game::ResetBall()
--- Game.h
+++ Game.h
@@ -9,8 +9,8 @@ class Game
 	Box paddle = {};
 
 	// TODO #1 - Instead of storing 1 brick, store a vector of bricks (by value)
-	//Box brick;
-	std::vector<Box> brick = {};
+	Box brick;
+	std::vector<Box> brickVector = {};
 
 public:
 	Game();
